1854e2f3f746d8babe2dd133e6463a5e110742ab
Maya <48359706+mayaesar@users.noreply.github.com>
2025-01-01 17:01:25 -0500

    fix console log

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,13 +1,11 @@
 import info from "@/mocks/instagram-data/personal_information/personal_information/personal_information.json"
 import profileImg from "@/mocks/instagram-data/media/profile/202408/457742621_1623914194834633_5168724281180364414_n_18136753804356985.jpg"
 import insight from "@/mocks/instagram-data/logged_information/past_instagram_insights/audience_insights.json"
-import following from "@/mocks/instagram-data/connections/followers_and_following/following.json"
 import Image from "next/image";
 
 export default function Hero() {
     const user = info.profile_user[0];
     const followers = insight.organic_insights_audience[0].string_map_data.Followers.value;
-    console.log();
 
     return(
         <section className="py-4">
